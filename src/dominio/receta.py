e3c8631271db339aae3e18ba81a0392918bfe2e2
lautaroleiva2000 <lautaroleiva2000@gmail.com>
2026-09-09 18:20:40 -0300

    Armar catálogo de recetas para E1

--- src/dominio/receta.py
+++ src/dominio/receta.py
@@ -1,1 +1,5 @@
-CATALOGO = []
+CATALOGO = [
+    {"id": 1, "nombre": "Chimichurri", "tiempo_min": 15, "dificultad": "baja"},
+    {"id": 2, "nombre": "Salsa criolla", "tiempo_min": 20, "dificultad": "baja"},
+    {"id": 3, "nombre": "Sofrito", "tiempo_min": 25, "dificultad": "baja"},
+]
