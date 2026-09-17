89286f36456d0b63d3461563aa17019103cb0715
lautaroleiva2000 <lautaroleiva2000@gmail.com>
2026-09-17 08:43:03 -0300

    E2: agregar clase Receta

--- src/dominio/receta.py
+++ src/dominio/receta.py
@@ -3,3 +3,14 @@ CATALOGO = [
     {"id": 2, "nombre": "Salsa criolla", "tiempo_min": 20, "dificultad": "baja"},
     {"id": 3, "nombre": "Sofrito", "tiempo_min": 25, "dificultad": "baja"},
 ]
+
+
+class Receta:
+    def __init__(self, id_receta, nombre, tiempo_min, dificultad):
+        self.id = id_receta
+        self.nombre = nombre
+        self.tiempo_min = tiempo_min
+        self.dificultad = dificultad
+
+    def resumen(self):
+        return f"{self.id}. {self.nombre} - {self.tiempo_min} min - Dificultad: {self.dificultad}"
