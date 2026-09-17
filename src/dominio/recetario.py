14e8aa6880b13adcd5366fc35aa977bfda0e881e
lautaroleiva2000 <lautaroleiva2000@gmail.com>
2026-09-17 19:35:10 -0300

    E2: implementar recursión de subrecetas

--- src/dominio/recetario.py
+++ src/dominio/recetario.py
@@ -4,6 +4,9 @@ from src.dominio.receta import Receta, CATALOGO
 class Recetario:
     def __init__(self):
         self.recetas = []
+        self.relaciones = {
+            10: [3, 5]
+        }        
 
         for datos in CATALOGO:
             receta = Receta(
@@ -23,3 +26,20 @@ class Recetario:
             if receta.id == id_receta:
                 return receta
         return None
+
+    def subrecetas(self, id_receta):
+        return self.relaciones.get(id_receta, [])
+
+
+    def desglosar_subrecetas(self, id_receta):
+        sub = self.subrecetas(id_receta)
+
+        if not sub:
+            return [id_receta]
+
+        resultado = [id_receta]
+
+        for s in sub:
+            resultado += self.desglosar_subrecetas(s)
+
+        return resultado
